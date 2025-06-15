2749b2128f7fb84f67d641b140f54a27a2bbacb0
mikelMondragon <132443700+mikelMondragon@users.noreply.github.com>
2025-06-15 12:03:55 +0200

    data base creation

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const { authRoutes, publicRoutes } = require("./routes/index.routes")
-
-
+const { createDB } = require("./utils/DBcreate")
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -16,6 +15,13 @@ app.use("/auth", authRoutes);
 
 app.use("/api/v1", publicRoutes);
 
+
+createDB();
+
+
 app.listen(port, () => {
     console.log(`server run on port: ${port}`)
 })
+
+
+
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,5 @@
-const login = async (req, res) => {
-    res.status(200).json({
-        ok: true
-    })
-}
+const { queryDB } = require("../utils/DBquery")
+const login = async (req, res) => { }
 const registry = async (req, res) => { }
 const renewToken = async (req, res) => { }
 const validateRole = async (req, res) => { }
