628cca4f0c6efd607af35de0f0c36bb43fb73b8e
sebastian438 <sebastian07051999@gmail.com>
2025-06-16 21:03:55 +0200

    ADD: script DBinit implementado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 require("dotenv").config();
 const { authRoutes, publicRoutes } = require("./routes/index.routes")
-const { createDB } = require("./utils/DBcreate")
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -21,10 +20,3 @@ app.use("/api/v1", publicRoutes);
 app.listen(port, () => {
     console.log(`server run on port: ${port}`)
 })
-
-const temp = async () => {
-    await createDB();
-}
-
-
-temp()
