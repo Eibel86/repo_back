0d4ef384f89e5e41855a92c94f8c1ed5a8b6c81c
sebastian438 <sebastian07051999@gmail.com>
2025-06-21 11:23:44 +0200

    ADD; erro del controlador login modificado

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -28,7 +28,7 @@ const login = async (req, res) => {
         //2. Si no existe el usuario
         if (!user) {
             return res.status(404).json({
-                error: "Usuario no encontrado"
+                error: "Usuario o contraseña incorrecta"
             });
         }
 
@@ -36,7 +36,7 @@ const login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(401).json({
-                error: "Contraseña incorrecta"
+                error: "Usuario o contraseña incorrecta"
             });
         }
 
